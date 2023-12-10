d944a7557e1d7580e8f738fca82245a749f75473
xSaadRM <151461794+xSaadRM@users.noreply.github.com>
2023-12-10 14:45:00 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,17 +1,41 @@
-const fs = require('fs');
 const { Client, LocalAuth } = require('whatsapp-web.js');
-const qrcode = require('qrcode-terminal');
+const fs = require('fs');
 
-const client = new Client({ 
+// Path where the session data will be stored
+const SESSION_FILE_PATH = './session.json';
 
-    authStrategy: new LocalAuth()
+// Load the session data if it has been previously saved
+let sessionData;
+if (fs.existsSync(SESSION_FILE_PATH)) {
+    sessionData = require(SESSION_FILE_PATH);
+}
+
+// Create a client with LocalAuth for session persistence
+const client = new Client({
+    authStrategy: new LocalAuth({
+        sessionData: sessionData // Pass the loaded session data
+    })
 });
 
-client.on('qr', qr => {
-    qrcode.generate(qr, { small: true });
-    console.log('Please scan the QR code with your phone.');
+// Save session values to the file upon successful auth
+client.on('authenticated', (session) => {
+    sessionData = session;
+    fs.writeFile(SESSION_FILE_PATH, JSON.stringify(session), (err) => {
+        if (err) {
+            console.error(err);
+        }
+    });
 });
 
+// Listen for the QR code and authenticate
+client.on('qr', (qr) => {
+    console.log('Scan this QR code with your phone:', qr);
+});
 
+// Listen for the client to be ready
+client.on('ready', () => {
+    console.log('Client is ready!');
+});
 
+// Initialize the client
 client.initialize();
